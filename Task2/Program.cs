58a03b7864eb66784c65bb87fca4cddf84ce88f5
TatianaPerfileva <byhgalter22@mail.ru>
2022-11-17 18:23:21 +0700

    add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = new Random().Next(-100, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(-100, 100);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(-100, 100);
+Console.WriteLine(numberC);
+int max = -100;
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+    Console.Write("max = ");
+    Console.WriteLine(max);
