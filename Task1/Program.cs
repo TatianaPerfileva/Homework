5b075356f6ec2884f50583d87f6c1e8423fba7a6
TatianaPerfileva <byhgalter22@mail.ru>
2022-11-17 15:42:16 +0700

    add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberOne = new Random().Next(-100, 100);
+Console.WriteLine(numberOne);
+int numberSecond = new Random().Next(-100, 100);
+Console.WriteLine(numberSecond);
+
+if (numberOne > numberSecond)
+{
+    Console.Write("max ");
+    Console.WriteLine(numberOne);
+    Console.Write("min ");
+    Console.WriteLine(numberSecond);
+}
+else
+{
+    Console.Write("max ");
+    Console.WriteLine(numberSecond);
+    Console.Write("min ");
+    Console.WriteLine(numberOne);
+}
