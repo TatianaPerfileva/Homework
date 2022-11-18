406af53b3e3200c91fee57f80bc7c159d2a7b17b
TatianaPerfileva <byhgalter22@mail.ru>
2022-11-18 17:15:03 +0700

    add Task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,19 +1,12 @@
-﻿Console.WriteLine("Введите число больше 0");
+﻿Console.WriteLine("Введите положительное число");
 int number = Convert.ToInt32(Console.ReadLine());
-int count = 0;
+int count = 2;
 
 while (count <= number)
-    { 
-          if (number %2 == 0)  
-        {
-          Console.Write(count + " "); 
-          count++;
-        }
-
-          else
-          {
-          count++;
-          } 
-    }  
-
+{
+         
+    Console.Write(count + " ");
+    count = count + 2;
+         
+}
 Console.WriteLine();
