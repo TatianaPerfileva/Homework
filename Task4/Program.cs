0f6e373cf2796e886491072dda6ca37daf763759
TatianaPerfileva <byhgalter22@mail.ru>
2022-11-18 16:05:46 +0700

    add Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число больше 0");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (count <= number)
+    { 
+          if (number %2 == 0)  
+        {
+          Console.Write(count + " "); 
+          count++;
+        }
+
+          else
+          {
+          count++;
+          } 
+    }  
+
+Console.WriteLine();
