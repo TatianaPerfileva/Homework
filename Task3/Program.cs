e737d837e7b8db5d17fc038fe072f7b1bc4d7d2d
TatianaPerfileva <byhgalter22@mail.ru>
2022-11-18 12:41:42 +0700

    add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new Random().Next(-100, 100);
+//Console.WriteLine(number);
+if (number % 2 == 0)
+    {
+        Console.WriteLine($"{number} -четное число");
+    }
+else
+    {
+    Console.WriteLine($"{number}-нечетное число");
+    }
